33e828004d75d318258cad4059c1ae4f03c7b617
Robert Hicks <darclan28@yahoo.com>
2011-07-13 08:17:15 -0500

    added RoR directory tree

---
+++ c-school/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSchool::Application.config.secret_token = '15438eb27a632a9cba63567ca8bb24615978783741d57593448d11888d6f1272158d88274a9879e243e73e61eda1a226b7b1d67f8cf4946864fd677900eaa29c'
